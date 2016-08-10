10c9757e88c570c95491677411cb281b9970453c
Tony Mai <tonymaibox@yahoo.com>
2016-08-10 19:22:08 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+	get '/name' do
+		"My name is Tony"
+	end
+
+	get '/hometown' do
+		"My hometown is Brooklyn, NY"
+	end
+
+	get '/favorite-song' do
+		"My favorite song is (too many to list)"
+	end
+
 end
